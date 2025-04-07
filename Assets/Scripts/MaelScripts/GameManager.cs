f26e7320dc6270003fcb1bb35fed38ef763ba103
Elmrys <elmrys@gmail.com>
2025-04-07 22:48:10 +0200

    small change

--- Assets/Scripts/MaelScripts/GameManager.cs
+++ Assets/Scripts/MaelScripts/GameManager.cs
@@ -341,6 +341,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //================================//
     public void EnterRoom(int roomIndex)
     {
         Debug.Log("Entering room: " + roomIndex);
